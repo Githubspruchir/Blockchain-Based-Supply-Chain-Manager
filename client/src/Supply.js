ddd75db5da18ac9ebd47e65df9d28783467babb0
Githubspruchir <159113133+Githubspruchir@users.noreply.github.com>
2024-12-31 19:06:00 +0530

    Update Supply.js

--- client/src/Supply.js
+++ client/src/Supply.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useHistory } from "react-router-dom"
 import Web3 from "web3";
 import SupplyChainABI from "./artifacts/SupplyChain.json"
+import './Supply.css'
 
 function Supply() {
     const history = useHistory()
